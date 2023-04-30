a6933df1b2956ddfc0353d075c51ea6ba6aca113
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-04-30 18:37:01 +0300

    editing readmi file

--- src/components/ImagesGallery.jsx
+++ src/components/ImagesGallery.jsx
@@ -4,6 +4,7 @@ import { casesImages } from 'constants/casesImges';
 import prev from 'images/gallery/prev.png';
 import next from 'images/gallery/next.png';
 import close from 'images/gallery/close.png';
+import loading from 'images/gallery/loading.gif';
 
 const ImageGallery = () => {
   const countImages = casesImages.length;
@@ -11,13 +12,22 @@ const ImageGallery = () => {
   const viewImage = (image, i) => {
     setData({ image, i });
   };
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadingFunk = async () => {
+    setIsLoading(true);
+    await new Promise(resolve => setTimeout(resolve, 300));
+    setIsLoading(false);
+  };
 
   const imgAction = action => {
     let i = data.i;
     if (action === 'next-img') {
+      loadingFunk();
       setData({ image: casesImages[i + 1], i: i + 1 });
     }
     if (action === 'prev-img') {
+      loadingFunk();
       setData({ image: casesImages[i - 1], i: i - 1 });
     }
     if (!action) {
@@ -29,45 +39,48 @@ const ImageGallery = () => {
     <div className="gallery">
       {data.image && (
         <div className="backdrop">
-          <div className="gallery_modal-content">
-            {data.i !== 0 && (
-              <button
-                className="gallery_arrow-prev"
-                type="button"
-                onClick={() => imgAction('prev-img')}
-              >
-                <img src={prev} alt="previous" />
-              </button>
-            )}
-            {data.i < countImages - 1 && (
-              <button
-                className="gallery_arrow-next"
-                type="button"
-                onClick={() => imgAction('next-img')}
-              >
-                <img src={next} alt="next" />
-              </button>
-            )}
+          {isLoading && <img src={loading} alt="loading" />}
+          {!isLoading && (
+            <div className="gallery_modal-content">
+              {data.i !== 0 && (
+                <button
+                  className="gallery_arrow-prev"
+                  type="button"
+                  onClick={() => imgAction('prev-img')}
+                >
+                  <img src={prev} alt="previous" />
+                </button>
+              )}
+              {data.i < countImages - 1 && (
+                <button
+                  className="gallery_arrow-next"
+                  type="button"
+                  onClick={() => imgAction('next-img')}
+                >
+                  <img src={next} alt="next" />
+                </button>
+              )}
 
-            <button
-              className="gallery_arrow-close"
-              type="button"
-              onClick={() => imgAction()}
-            >
-              <img src={close} alt="close" />
-            </button>
-            <picture className="gallery_image">
-              <source
-                srcSet={`${data.image.casesJpg} 1x, ${data.image.casesJpg2x} 2x`}
-                type="image/webp"
-              />
-              <source
-                srcSet={`${data.image.casesWebp} 1x, ${data.image.casesWebp2x} 2x`}
-                type="image/jpeg"
-              />
-              <img src={data.image.casesJpg} alt="case" />
-            </picture>
-          </div>
+              <button
+                className="gallery_arrow-close"
+                type="button"
+                onClick={() => imgAction()}
+              >
+                <img src={close} alt="close" />
+              </button>
+              <picture className="gallery_image">
+                <source
+                  srcSet={`${data.image.casesJpg} 1x, ${data.image.casesJpg2x} 2x`}
+                  type="image/webp"
+                />
+                <source
+                  srcSet={`${data.image.casesWebp} 1x, ${data.image.casesWebp2x} 2x`}
+                  type="image/jpeg"
+                />
+                <img src={data.image.casesJpg} alt="case" />
+              </picture>
+            </div>
+          )}
         </div>
       )}
 
